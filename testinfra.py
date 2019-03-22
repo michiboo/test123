f4800980454ba90c2d7bcd712559dfe0d8b2b401
Micky Yun Chan <chanmickyyun@gmail.com>
2019-03-23 00:40:35 +0200

    Update testinfra.py

--- testinfra.py
+++ testinfra.py
@@ -1,17 +1,27 @@
-def test_puppet_is_installed(host):
-    nginx = host.package("puppet")
-    assert nginx.is_installed
+import unittest
+import testinfra
 
-def test_puppet_ser_is_installed(host):
-    nginx = host.package("puppetserver")
-    assert nginx.is_installed
+class docker_Test(unittest.TestCase):
+    
+    def setUp(self):
+        self.host = testinfra.get_host("paramiko://root@host")
 
-def test_rsyslog_running_and_enabled(host):
-    rsyslog = host.service("rsyslog")
-    assert rsyslog.is_running
-    assert rsyslog.is_enabled
+    def test_puppet_is_installed(host):
+        puppet = host.package("puppet")
+        assert puppet.is_installed
 
-def test_ssh_running_and_enabled(host):
-    ssh = host.service("ssh")
-    assert ssh.is_running
-    assert ssh.is_enabled
+    def test_puppet_ser_is_installed(host):
+        puppet_ser = host.package("puppetserver")
+        assert puppet_ser.is_installed
+
+    def test_rsyslog_running_and_enabled(host):
+        rsyslog = host.service("rsyslog")
+        assert rsyslog.is_running
+        assert rsyslog.is_enabled
+
+    def test_ssh_running_and_enabled(host):
+        ssh = host.service("ssh")
+        assert ssh.is_running
+        assert ssh.is_enabled
+if __name__ == "__main__":
+    unittest.main()
