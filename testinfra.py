0f5cb24ebdfc09a38e129034d791d866334d7309
Micky Yun Chan <chanmickyyun@gmail.com>
2019-03-23 10:09:37 +0200

    Update testinfra.py

--- testinfra.py
+++ testinfra.py
@@ -1,26 +1,25 @@
 import unittest
 import testinfra
-
+# need to add RUN yum -y install initscripts
 class docker_Test(unittest.TestCase):
     
     def setUp(self):
         self.host = testinfra.get_host("docker://[user]@[id]") #id for basic_config_master container
 
-    def test_puppet_is_installed(host):
-        puppet = host.package("puppet")
-        assert puppet.is_installed
+    def test_puppet_is_installed(self):
+        assert self.host.check_output('puppet -v') == "5.5.10"
 
-    def test_puppet_ser_is_installed(host):
-        puppet_ser = host.package("puppetserver")
-        assert puppet_ser.is_installed
+    def test_puppet_ser_is_installed(self):
+        assert self.host.check_output('puppet agent --version') == "5.5.10"
+        assert self.host.check_output('puppet master --version') == "5.5.10"
 
-    def test_rsyslog_running_and_enabled(host):
-        rsyslog = host.service("rsyslog")
+    def test_rsyslog_running_and_enabled(self):
+        rsyslog = self.host.service("rsyslog")
         assert rsyslog.is_running
         assert rsyslog.is_enabled
 
-    def test_ssh_running_and_enabled(host):
-        ssh = host.service("ssh")
+    def test_ssh_running_and_enabled(self):
+        ssh = self.host.service("ssh")
         assert ssh.is_running
         assert ssh.is_enabled
 if __name__ == "__main__":
