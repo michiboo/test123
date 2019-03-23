90ad00dd76ea355a15ae6939f6ecc66ae70d37bc
Micky Yun Chan <chanmickyyun@gmail.com>
2019-03-23 18:21:43 +0200

    Update testinfra.py

--- testinfra.py
+++ testinfra.py
@@ -1,22 +1,25 @@
+# for some reason path variable is not same container if connect from testinfra
+
+path = 'export PATH="/usr/sue/sbin:/usr/sue/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin" && '
+
 import unittest
 import testinfra
-# need to add RUN yum -y install initscripts
 class docker_Test(unittest.TestCase):
     
     def setUp(self):
         self.host = testinfra.get_host("docker://root@[id]") #id for basic_config_master container
 
     def test_puppet_is_installed(self):
-        assert self.host.check_output('puppet -v') == "5.5.10"
+        assert self.host.check_output(path + 'puppet --version') == "5.5.10"
 
     def test_puppet_ser_is_installed(self):
-        assert self.host.check_output('puppet agent --version') == "5.5.10"
-        assert self.host.check_output('puppet master --version') == "5.5.10"
+        assert self.host.check_output(path + 'puppet agent --version') == "5.5.10"
+        assert self.host.check_output(path + 'puppet master --version') == "5.5.10"
 
     def test_rsyslog_running(self):
-        assert self.host.check_output('pgrep -x rsyslogd')  
+        assert self.host.check_output(path + 'pgrep -x rsyslogd')  
 
     def test_ssh_running(self):
-        assert self.host.check_output('pgrep -x sshd')  
+        assert self.host.check_output(path + 'pgrep -x sshd')  
 if __name__ == "__main__":
     unittest.main()
